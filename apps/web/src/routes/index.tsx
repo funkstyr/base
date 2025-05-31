5bcd8b32845133d2e087c51179a3fd37afcc2a5b
Michael Funk <funkstyr@gmail.com>
2025-05-31 15:12:59 -0400

    feat: center title

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -7,8 +7,8 @@ export const Route = createFileRoute("/")({
 const TITLE_TEXT = `
 ██████╗  █████╗  ██████╗ ███████╗
 ██╔══██╗██╔══██╗██╔════╝ ██╔════╝
-██████╔╝███████║╚█████╗  █████╗
-██╔══██╗██╔══██║ ╚═══██╗ ██╔══╝
+██████╔╝███████║╚█████╗  █████╗  
+██╔══██╗██╔══██║ ╚═══██╗ ██╔══╝  
 ██████╔╝██║  ██║██████╔╝ ███████╗
 ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚══════╝
 `;
@@ -16,7 +16,7 @@ const TITLE_TEXT = `
 function HomeComponent() {
   return (
     <div className="center container mx-auto max-w-3xl px-4 py-2">
-      <pre className="center overflow-x-auto pb-2 font-mono text-sidebar-primary text-sm">
+      <pre className="w-full overflow-x-auto pb-2 text-center font-mono text-sidebar-primary text-sm">
         {TITLE_TEXT}
       </pre>
     </div>
