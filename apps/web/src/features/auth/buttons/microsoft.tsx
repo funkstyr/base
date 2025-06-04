b21f98b599493111503ebf11cacc0dfe419ed7e7
funkstyr <funkstyr@gmail.com>
2025-06-04 18:18:49 -0400

    refactor: pull auth buttons to their own components

---
+++ apps/web/src/features/auth/buttons/microsoft.tsx
@@ -0,0 +1,31 @@
+import { auth } from "@base/auth/client/web";
+import { Button } from "@base/ui/components/button";
+import { cn } from "@base/ui/lib/utils";
+
+export function MicrosoftButton() {
+  return (
+    <Button
+      variant="outline"
+      className={cn("flex-grow")}
+      onClick={async () => {
+        await auth.signIn.social({
+          provider: "microsoft",
+          callbackURL: "/dashboard",
+        });
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <title>microsoft logo</title>
+        <path
+          fill="currentColor"
+          d="M2 3h9v9H2zm9 19H2v-9h9zM21 3v9h-9V3zm0 19h-9v-9h9z"
+        />
+      </svg>
+    </Button>
+  );
+}
--- apps/web/src/features/auth/sign-in-form.tsx
+++ apps/web/src/features/auth/sign-in-form.tsx
@@ -9,6 +9,10 @@ import { Button } from "@base/ui/components/button";
 import { Input } from "@base/ui/components/input";
 import { Label } from "@base/ui/components/label";
 import { Loader } from "@base/ui/components/loader";
+import { AppleButton } from "./buttons/apple";
+import { DiscordButton } from "./buttons/discord";
+import { GoogleButton } from "./buttons/google";
+import { MicrosoftButton } from "./buttons/microsoft";
 
 export default function SignInForm({
   onSwitchToSignUp,
@@ -135,112 +139,10 @@ export default function SignInForm({
             "flex-wrap justify-between",
           )}
         >
-          <Button
-            variant="outline"
-            className={clsx("flex-grow")}
-            onClick={async () => {
-              await auth.signIn.social({
-                provider: "apple",
-                callbackURL: "/dashboard",
-              });
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-            >
-              <title>apple logo</title>
-              <path
-                fill="currentColor"
-                d="M17.05 20.28c-.98.95-2.05.8-3.08.35c-1.09-.46-2.09-.48-3.24 0c-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8c1.18-.24 2.31-.93 3.57-.84c1.51.12 2.65.72 3.4 1.8c-3.12 1.87-2.38 5.98.48 7.13c-.57 1.5-1.31 2.99-2.54 4.09zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25c.29 2.58-2.34 4.5-3.74 4.25"
-              />
-            </svg>
-          </Button>
-
-          <Button
-            variant="outline"
-            className={clsx("flex-grow")}
-            onClick={async () => {
-              await auth.signIn.social({
-                provider: "google",
-                callbackURL: "/dashboard",
-              });
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="0.98em"
-              height="1em"
-              viewBox="0 0 256 262"
-            >
-              <title>google logo</title>
-              <path
-                fill="#4285F4"
-                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-              />
-              <path
-                fill="#34A853"
-                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-              />
-              <path
-                fill="#FBBC05"
-                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
-              />
-              <path
-                fill="#EB4335"
-                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-              />
-            </svg>
-          </Button>
-          <Button
-            variant="outline"
-            className={clsx("flex-grow")}
-            onClick={async () => {
-              await auth.signIn.social({
-                provider: "microsoft",
-                callbackURL: "/dashboard",
-              });
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-            >
-              <title>microsoft logo</title>
-              <path
-                fill="currentColor"
-                d="M2 3h9v9H2zm9 19H2v-9h9zM21 3v9h-9V3zm0 19h-9v-9h9z"
-              />
-            </svg>
-          </Button>
-
-          <Button
-            variant="outline"
-            className={clsx("flex-grow")}
-            onClick={async () => {
-              await auth.signIn.social({
-                provider: "discord",
-                callbackURL: "/dashboard",
-              });
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-            >
-              <title>discord logo</title>
-              <path
-                fill="currentColor"
-                d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"
-              />
-            </svg>
-          </Button>
+          <GoogleButton />
+          <AppleButton />
+          <MicrosoftButton />
+          <DiscordButton />
         </div>
       </form>
 
