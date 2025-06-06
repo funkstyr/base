1469f508dadb452c4dc54d22935278f7de479c92
funkstyr <funkstyr@gmail.com>
2025-06-05 23:24:11 -0400

    fix: add cookies to supabase client

--- packages/supabase/src/client/server.ts
+++ packages/supabase/src/client/server.ts
@@ -1,4 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
+import { parseCookies, setCookie } from "@tanstack/react-start/server";
 
 import type { Database } from "../types";
 
@@ -52,9 +53,19 @@ export async function createClient(options?: CreateClientOptions) {
   return createServerClient<Database>(process.env.SUPABASE_URL ?? "", key, {
     ...rest,
     cookies: {
-      //@ts-expect-error handle cookies
-      getAll: () => ({}),
-      setAll: () => ({}),
+      // @ts-ignore Wait till Supabase overload works
+      getAll() {
+        return Object.entries(parseCookies()).map(([name, value]) => ({
+          name,
+          value,
+        }));
+      },
+      setAll(cookies) {
+        for (const cookie of cookies) {
+          const { name, value, options } = cookie;
+          setCookie(name, value, options);
+        }
+      },
     },
     auth,
     global: {
