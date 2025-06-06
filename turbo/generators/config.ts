69a615655d211bfc10c000afd0b96094a6796c79
funkstyr <funkstyr@gmail.com>
2025-06-06 14:17:18 -0400

    feat: add initial templates

---
+++ turbo/generators/config.ts
@@ -0,0 +1,42 @@
+import type { PlopTypes } from "@turbo/gen";
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator("init", {
+    description: "Generate a new package for the Monorepo",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message:
+          "What is the name of the package? (You can skip the `@base/` prefix)",
+      },
+    ],
+    actions: [
+      (answers) => {
+        if (
+          "name" in answers &&
+          typeof answers.name === "string" &&
+          answers.name.startsWith("@base/")
+        ) {
+          answers.name = answers.name.replace("@base/", "");
+        }
+        return "Config sanitized";
+      },
+      {
+        type: "add",
+        path: "packages/{{ kebabCase name }}/package.json",
+        templateFile: "templates/init-pkg/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{ kebabCase name }}/tsconfig.json",
+        templateFile: "templates/init-pkg/tsconfig.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{ kebabCase name }}/src/index.ts",
+        templateFile: "templates/init-pkg/index.ts.hbs",
+      },
+    ],
+  });
+}
